7bc8587f0d8505fbd453d941a2ebe614bcdac904
Adrian Perez <appperez@ucdavis.edu>
2025-01-06 22:00:49 -0800

    Add functionality for creating a recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,26 @@
 class RecipesController < ApplicationController
   before_action :authenticate_user!
   def index
+    @recipes = current_user.recipes
+  end
 
+  def new
+    @recipe = current_user.recipes.build
+  end
+
+  def create
+    @recipe = current_user.recipes.build(recipe_params)
+
+    if @recipe.save
+      redirect_to recipes_path
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:description, :title, :food_img)
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,14 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_one_attached :food_img
+  after_initialize :attach_default_food_img
 
   validates :user_id, :description, :title, presence: true
+
+  private
+
+  def attach_default_food_img
+    food_img.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'default_food_img.png')), filename: 'default_food_img.png', content_type: 'image/png') unless food_img.attached?
+  end
+
 end
